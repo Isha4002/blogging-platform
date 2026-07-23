59da23dbd42a2043a86fb8de09b0e8f09147ece6
Isha Pal <ishapal2004@gmail.com>
2026-07-23 12:10:16 +0530

    Setup React routing and Tailwind CSS

---
+++ client/src/pages/BlogDetails.jsx
@@ -0,0 +1,5 @@
+function BlogDetails() {
+  return <h1>Blog Details</h1>;
+}
+
+export default BlogDetails;
---
+++ client/src/pages/CreateBlog.jsx
@@ -0,0 +1,5 @@
+function CreateBlog() {
+  return <h1>Create Blog</h1>;
+}
+
+export default CreateBlog;
