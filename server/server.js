3cfbac6df05e5d527381720170d18e5bf23aa01b
Isha Pal <ishapal2004@gmail.com>
2026-07-23 11:17:51 +0530

    Create user model and auth structure

--- server/server.js
+++ server/server.js
@@ -2,19 +2,21 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const connectDB = require("./config/db");
+
+connectDB();
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({
-    message: "Blog API is running..."
-  });
+    res.json({ message: "Blog API running..." });
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
